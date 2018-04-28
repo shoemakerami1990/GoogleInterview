a5730548fde6e09f21f11890b15f1581f0514947
shoemakerami1990 <shoemakerami1990@gmail.com>
2018-04-28 09:34:38 -0700

    581. fixes

--- leetcode/581.shortest-unsorted-continuous-subarray.py
+++ leetcode/581.shortest-unsorted-continuous-subarray.py
@@ -5,24 +5,24 @@ class Solution(object):
         :rtype: int
         """
         n = len(nums)
-        l, i = 0, 0
+        l, i = n-1, 0
         for i in range(1, n):
             if nums[i]<nums[i-1]:
-                l = i
+                l = i-1
                 break
 
-        for i in range(i, n):
-            while l>=0 and nums[i]<nums[l]:
+        for i in range(i+1, n):
+            while l>0 and nums[i]<nums[l-1]:
                 l-=1
 
-        r, i = n-1, n-1
+        r, i = 0, n-1
         for i in range(n-2, -1, -1):
-            if nums[i]<nums[i+1]:
-                r = i
+            if nums[i]>nums[i+1]:
+                r = i+1
                 break
 
-        for i in range(i, -1, -1):
-            while r<n and nums[i]>nums[r]:
+        for i in range(i-1, -1, -1):
+            while r<n-1 and nums[i]>nums[r+1]:
                 r+=1
 
-        return r-l+1
+        return r-l+1 if r-l>0 else 0
